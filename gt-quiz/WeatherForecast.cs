8b3e18c4c41e7b009bf262a9ad49252ee405b1a8
ifyousayso <gplay@foshin.com>
2022-12-17 11:55:42 +0100

    Initial commit

---
+++ gt-quiz/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿namespace gt_quiz;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
+
